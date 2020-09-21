cc9a8a9be7c6b5af8529c0ce4787df5341024b1a
Gil Levi <46998962+gilevi@users.noreply.github.com>
2020-09-22 01:26:59 +0300

    Updates

---
+++ precache-manifest.7fcacd4d66986003afc9472a9f685996.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f2bd9ce4b6f9423c092cb3e2fb0d84",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "13983f1e47e385aef8b7",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "ee4a3028407b2bd5d652",
+    "url": "/material-dashboard-react/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "13983f1e47e385aef8b7",
+    "url": "/material-dashboard-react/static/js/2.6955075e.chunk.js"
+  },
+  {
+    "revision": "30c2c6c6407abdf31ce9809ecc70b99c",
+    "url": "/material-dashboard-react/static/js/2.6955075e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4a3028407b2bd5d652",
+    "url": "/material-dashboard-react/static/js/main.53b0b0c6.chunk.js"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "88d95eb233422753e1cceaede9c9ff09",
+    "url": "/material-dashboard-react/static/media/message.88d95eb2.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
