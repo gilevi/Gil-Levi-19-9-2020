1239f72aa60c3d425d280306a112b26334e5cc6b
Gil Levi <46998962+gilevi@users.noreply.github.com>
2020-10-01 13:43:57 +0300

    Delete comments and console.log

--- message-system/src/actions/index.js
+++ message-system/src/actions/index.js
@@ -41,46 +41,12 @@ export const LoginInto = (data) => async dispatch => {
     console.log(request)
     var ifLogin = false;
     if(request.status === 201){
-        // setLogin(true);
-        // var isLogin = JSON.parse("true".toLowerCase());
-        // sessionStorage.setItem('login', true); 
         ifLogin = true;
-
-    }
-    else if(request.status === 400) {
-        ifLogin = false;
-
-    }
-    else{
-        // setLogin(false);
-        // var isLogin = JSON.parse("false".toLowerCase());
-        // sessionStorage.setItem('login', false); 
-        ifLogin = false;
     }
     return {
         type:'LOGIN_USER',
         payload: ifLogin
     }
 
-    // fetch('http://167.172.162.59:9000/users/login', {
-    //     method: 'POST',
-    //     headers:  new Headers ({
-    //         'Content-Type': 'application/x-www-form-urlencoded',
-    //         'Accept': 'application/json, text/plain, */*'
-    //     }),
-    //     body: JSON.stringify({ user: username, password: password})
-    //     }).then(data => {
-    //         console.log('Success:', data.status);
-    //         if(data.status === 201){
-    //             setLogin(true);
-    //             var isLogin = JSON.parse("true".toLowerCase());
-    //             sessionStorage.setItem('login', isLogin); 
-
-    //         }
-    //         else{
-    //             setLogin(false);
-    //             var isLogin = JSON.parse("true".toLowerCase());
-    //             sessionStorage.setItem('login', false); 
-    //         }
-    //       })
+   
 };
